f5714ec9185255eff49e3a9dd65426438604e5d8
Sami <islam.samipkr@gmail.com>
2022-12-08 22:13:37 -0500

    Initial Commit of the code

--- lib/main.dart
+++ lib/main.dart
@@ -11,29 +11,18 @@ void main() {
    Widget build(BuildContext context){
     return MaterialApp(
       home:Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: Center(
-              child: Text('Ghotok')),
-        ),
-        backgroundColor: Colors.blue,
+          backgroundColor: Colors.teal,
           body: SafeArea(
-            child: Container(
-              height: 100.0,
-              width:100.0,
-              margin:EdgeInsets.all(20.0),
-              child: Center(
-                child: Text("The name of this app is called Ghotok")
-              ),
-              color:Colors.tealAccent
-            ),
+            child: Column(
+              children: <Widget>[
+                 CircleAvatar(
+                  radius:50.0,
+                  backgroundImage: AssetImage('images/Sami_Islam_last.jpg'),
+            )
+            ],
           ),
-        floatingActionButton: FloatingActionButton(
-          child: Text('Click on it'),
-          backgroundColor: Colors.black, onPressed: () { }
-          ,
+          ),
         ),
-      )
-    );
+      );
   }
  }
